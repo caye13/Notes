9380bfa89289d3ce781627e61e95605cff35301b
caye13 <caye1300@gmail.com>
2020-08-04 15:40:58 -0700

    change view and text field

--- Notes/Controllers/DisplayNoteViewController.swift
+++ Notes/Controllers/DisplayNoteViewController.swift
@@ -12,5 +12,35 @@ class DisplayNoteViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleTextField.text = ""
+        contentTextView.text = ""
+        
     }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier else { return }
+
+        switch identifier {
+        case "save":
+            let note = Note()
+            
+            note.title = titleTextField.text ?? ""
+            note.content = contentTextView.text ?? ""
+            note.modificationTime = Date()
+            
+            let destination = segue.destination as! ListNotesTableViewController
+            destination.notes.append(note)
+
+        case "cancel":
+            print("cancel bar button item tapped")
+
+        default:
+            print("unexpected segue identifier")
+        }
+    }
+    
 }
