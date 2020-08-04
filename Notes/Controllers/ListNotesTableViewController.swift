abd1e4dca5295defb128b85aae1c1f0b7ab75e06
caye13 <caye1300@gmail.com>
2020-08-04 12:10:24 -0700

    edits

--- Notes/Controllers/ListNotesTableViewController.swift
+++ Notes/Controllers/ListNotesTableViewController.swift
@@ -13,4 +13,15 @@ class ListNotesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath)
+        cell.textLabel?.text = "Cell Row: \(indexPath.row) Section: \(indexPath.section)"
+
+        return cell
+    }
 }
